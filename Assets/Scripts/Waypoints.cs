9dcac4c92641f652723ec0cc1139c068076edd05
jackie saltalepre <pirrulez@gmail.com>
2020-09-06 19:19:05 +0100

    end E02

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+    
+    // Static variable so that we can access it from another script without needing to instantiate Waypoints class
+    public static Transform[] points;
+
+    void Awake() {
+    	points = new Transform[transform.childCount];
+    	for(int i = 0; i < points.Length; i++)
+    		points[i] = transform.GetChild(i);
+    }
+}
