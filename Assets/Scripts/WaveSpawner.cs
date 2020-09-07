60bee5f913dd41016d48ae1cd93e65a5a4f3b40b
jackie saltalepre <pirrulez@gmail.com>
2020-09-07 10:05:32 +0100

    end E03

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+// Lets us use IEnumerator and Coroutines
+using System.Collections;
+
+// Lets us reference a Text
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public Text waveCountdownText;
+    public float timeBetweenWaves = 5f;
+
+    private float countdown = 2f;
+    private int waveIndex = 0;
+
+    void Update() {
+    	if (countdown <= 0f) {
+    		StartCoroutine(SpawnWave());
+    		countdown = timeBetweenWaves;
+    	}
+
+    	countdown -= Time.deltaTime;
+
+    	waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave() {
+    	waveIndex++;
+
+    	for(int i = 0; i < waveIndex; i++) {
+    		SpawnEnemy();
+    		yield return new WaitForSeconds(0.5f);
+    	}
+    }
+
+    void SpawnEnemy() {
+    	Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
