8fb98b58bfaa3e1b5f77123a50ed9db90973cc3b
jackie saltalepre <pirrulez@gmail.com>
2020-09-10 20:04:49 +0100

    end E10

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,9 @@
 
 public class Bullet : MonoBehaviour {
 	private Transform target;
+
 	public float speed = 70f;
+	public float explosionRadius = 0f;
 	public GameObject impactEffect;
 
 	// To be called by the turret that instantiates this bullet to pass it its target
@@ -28,17 +30,61 @@ public class Bullet : MonoBehaviour {
 
 		// No hit yet - move the bullet, relative to the world space (not the local one)
 		transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+		// Rotate the bullet to always face the target
+		transform.LookAt(target);
 	}
 
 	void HitTarget() {
 		// Spawn particles
 		GameObject effectIns = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
 
-		// Destroy the particles after 2 seconds to not waste memory
-		Destroy(effectIns, 2f);
+		// Destroy the particles after 5 seconds to not waste memory
+		Destroy(effectIns, 5f);
 
-		// Destroy the enemy and the bullet on impact
-		Destroy(target.gameObject);
+		// AoE
+		if (explosionRadius > 0f) {
+			Explode();
+		}
+
+		// Single target
+		else {
+			Damage(target);
+		}
+
+		// Destroy the bullet on impact
 		Destroy(gameObject);
 	}
+
+	void Damage(Transform enemy) {
+		Destroy(enemy.gameObject);
+	}
+
+	void Explode() {
+		// Creates a sphere at the given position and returns every gameOobject it overlaps with
+		Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+		foreach (Collider collider in colliders) 
+			if (collider.tag == "Enemy")
+				Damage(collider.transform);
+	}
+
+	// When selecting the bullet draws a wire sphere (just the outline) around the bullet of radius explosionRadius, not visible during the game
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, explosionRadius);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
