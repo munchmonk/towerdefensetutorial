0a3e0279697c49bca810664d21340ddeebfb2769
jackie saltalepre <pirrulez@gmail.com>
2020-09-10 01:18:24 +0100

    end E06

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	// Singleton pattern
+	public static BuildManager instance;
+
+	private GameObject turretToBuild;
+	public GameObject standardTurretPrefab;
+
+	void Awake() {
+		if (instance != null) {
+			Debug.Log("More than one BuildManager in Scene (should be a singleton!)");
+			return;
+		}
+		instance = this;
+	}
+
+	void Start() {
+		turretToBuild = standardTurretPrefab;
+	}
+
+	public GameObject GetTurretToBuild() {
+		return turretToBuild;
+	}
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+	public Color hoverColor;
+	public Vector3 positionOffset;
+
+	private Renderer rend;
+	private Color startColor;
+
+	private GameObject turret;
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+		startColor = rend.material.color;
+	}
+
+	void OnMouseDown() {
+		if (turret != null) {
+			Debug.Log("There is already a turret here!");
+			return;
+		}
+
+		GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+		turret = (GameObject) Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+	}
+
+	void OnMouseEnter() {
+		rend.material.color = hoverColor;
+	}
+
+	void OnMouseExit() {
+		rend.material.color = startColor;
+	}
+}
